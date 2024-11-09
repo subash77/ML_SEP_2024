f0ac698f0467e5f54fe1301e3c2a3dfe0fe07389
Subash Chelladurai <subashchella15@gmail.com>
2024-11-09 19:14:31 +0530

    Setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+
+    requirements = []
+    with open(file_path) as f:
+        requirements = f.readlines()
+        requirements =  [req.replace("\n","") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    
+    return requirements
+
+
+
+
+setup(
+    name = 'Machine Learning',
+    version = '0.01',
+    author = 'Subash',
+    author_email = 'subashchella15l@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
